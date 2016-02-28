2aaac148f6503d4a8607d5a9d9b433244fde42f9
Mike <kjyoung@ualberta.ca>
2016-02-28 00:09:14 -0700

    got something working, need to improve alot

--- mnist_test.m
+++ mnist_test.m
@@ -1,5 +1,16 @@
-clear
-%load mnist data
-display 'loading data...'
-images = single(loadMNISTImages('data/t10k-images-idx3-ubyte'));
-labels = single(loadMNISTLabels('data/t10k-labels-idx1-ubyte'));
+function [] = mnist_test(net)
+    %load mnist data
+    display 'loading data...'
+    images = single(loadMNISTImages('data/t10k-images-idx3-ubyte'));
+    images = reshape(images, 28, 28, 1, []) ;
+    labels = single(loadMNISTLabels('data/t10k-labels-idx1-ubyte'));
+    labels = labels+1;
+
+    net.layers{end}.class = labels ;
+    res = [];
+    res = vl_simplenn(net, images, [], res, ...
+      'disableDropout', false, ...
+      'conserveMemory', false, ...
+      'sync', true)
+    
+end
--- mnist_train.m
+++ mnist_train.m
@@ -1,11 +1,11 @@
-function [net, info] = mnist_train(varargin)
+function [net, info] = mnist_train()
     %load mnist data
     display 'loading data...'
     images = single(loadMNISTImages('data/train-images-idx3-ubyte'));
     labels = single(loadMNISTLabels('data/train-labels-idx1-ubyte'))';
     image_data.data = images;
     image_data.id = 1:size(images,3);
-    image_data.labels = labels;
+    image_data.labels = labels+1;
     image_data.set = ones(size(labels));
     image_data.set(1:end/10) = 2;
     
@@ -16,15 +16,16 @@ function [net, info] = mnist_train(varargin)
     trainOpts.numEpochs = 15 ;
     trainOpts.continue = true ;
     trainOpts.useGpu = false ;
-    trainOpts.learningRate = 0.001 ;
+    trainOpts.learningRate = 0.03 ;
     trainOpts.expDir = 'data/mnist-experiment' ;
-    trainOpts = vl_argparse(trainOpts, varargin);
 
     net = initializeNetwork();
     [net,info] = cnn_train(net, imdb, @getBatch, trainOpts) ;
+    save net
 end
 
 function [images, labels] = getBatch(imdb, batch)
     images = imdb.images.data(:,:,batch);
+    images = reshape(images, 28, 28, 1, []) ;
     labels = imdb.images.labels(1,batch);
 end
