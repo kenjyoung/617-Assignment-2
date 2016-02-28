65f71207b14caaf6166a9e0c3d0a705cd6dbe92d
Mike <kjyoung@ualberta.ca>
2016-02-28 12:27:07 -0700

    basically implemented required network but seems to have possible issues, training is not going well, also need to add ensemble

--- mnist_test.m
+++ mnist_test.m
@@ -1,16 +1,23 @@
-function [] = mnist_test(net)
+function [accuracy] = mnist_test(net)
+    setup;
     %load mnist data
     display 'loading data...'
     images = single(loadMNISTImages('data/t10k-images-idx3-ubyte'));
     images = reshape(images, 28, 28, 1, []) ;
     labels = single(loadMNISTLabels('data/t10k-labels-idx1-ubyte'));
-    labels = labels+1;
+    labels = labels;
 
-    net.layers{end}.class = labels ;
+    net.layers{end} = struct('type', 'softmax') ;
     res = [];
     res = vl_simplenn(net, images, [], res, ...
-      'disableDropout', false, ...
+      'disableDropout', true, ...
       'conserveMemory', false, ...
-      'sync', true)
+      'sync', true);
+    score=res(end).x;
     
+    [~,class] = max(score,[],3);
+    class = reshape(class,size(labels));
+    class = class-1;
+    accuracy = nnz(class==labels)/size(labels,1);
+    fprintf('Test set accuracy: %f\n',accuracy);
 end
--- mnist_train.m
+++ mnist_train.m
@@ -1,26 +1,46 @@
 function [net, info] = mnist_train()
+    setup;
     %load mnist data
-    display 'loading data...'
+    display 'loading data...';
     images = single(loadMNISTImages('data/train-images-idx3-ubyte'));
+    im_width = size(images,2);
+    im_height = size(images,1);
     labels = single(loadMNISTLabels('data/train-labels-idx1-ubyte'))';
+    %augment data set with translated data
+    for i=1:size(images,3)
+        new_labels(1,4*(i-1)+1:4*(i-1)+4) = labels(1,i);
+        new_images(:,:,4*(i-1)+1) = [zeros(1,im_width);images(2:end,:,i)];
+        new_images(:,:,4*(i-1)+2) = [images(1:end-1,:,i);zeros(1,im_width)];
+        new_images(:,:,4*(i-1)+3) = [zeros(im_height,1), images(:,2:end,i)];
+        new_images(:,:,4*(i-1)+4) = [images(:,1:end-1,i), zeros(im_height,1)];
+    end
+    
+    images = cat(3,images,new_images);
+    labels = [labels,new_labels];
+    shuffle = randperm(size(images,3));
+    %shuffle data
+    images = images(:,:,shuffle);
+    labels = labels(shuffle);
     image_data.data = images;
     image_data.id = 1:size(images,3);
     image_data.labels = labels+1;
     image_data.set = ones(size(labels));
-    image_data.set(1:end/10) = 2;
+    image_data.set(1:floor(size(images,3)/20)) = 2;
     
     
     imdb.images = image_data;
     
     trainOpts.batchSize = 10 ;
-    trainOpts.numEpochs = 15 ;
+    trainOpts.numEpochs = 40 ;
     trainOpts.continue = true ;
     trainOpts.useGpu = false ;
     trainOpts.learningRate = 0.03 ;
+    trainOpts.weightDecay = 0.1 ;
+    trainOpts.momentum = 0 ;
     trainOpts.expDir = 'data/mnist-experiment' ;
 
     net = initializeNetwork();
-    [net,info] = cnn_train(net, imdb, @getBatch, trainOpts) ;
+    [net,info] = cnn_train(net, imdb, @getBatch, trainOpts);
     save net
 end
 
